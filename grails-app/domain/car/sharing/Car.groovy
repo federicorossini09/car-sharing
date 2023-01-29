c88ca061591e7833d2aa5445bf680e847145e998
Martin Rosas <martin.rosas@despegar.com>
2023-01-29 19:56:52 -0300

    deleted constructor and replaced it with constraint

--- grails-app/domain/car/sharing/Car.groovy
+++ grails-app/domain/car/sharing/Car.groovy
@@ -12,19 +12,8 @@ class Car {
     Date vtvExpirationDate
 
 
-    Car(String licensePlate, String year, String brand, String model, String variant, Date vtvExpirationDate){
-        if(!licensePlate)
-            throw new CarCreationException("License Plate is empty")
-        this.licensePlate = licensePlate
-        this.year = year
-        this.brand = brand
-        this.model = model
-        this.variant = variant
-        this.vtvExpirationDate = vtvExpirationDate
-    }
-
-
-
     static constraints = {
+        licensePlate nullable: false, blank: false
+
     }
 }
